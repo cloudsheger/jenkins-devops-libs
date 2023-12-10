76ff8de375a13d809fb1590ccfca3f4dfa4805f9
cloudsheger <cloudsheger@gmail.com>
2023-12-10 03:42:03 -0500

    dockerfile path updated

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -3,6 +3,6 @@
 def call(Map config) {
     def dockerImage = "${config.DOCKER_REGISTRY}/${config.DOCKER_REPO}/${config.IMAGE_NAME}:${config.BUILD_NUMBER}"
     // Build Docker image
-    sh "docker build -f docker/Dockerfile -t ${dockerImage} ."
+    sh "docker build -f dockerfiles/Dockerfile.packer -t ${dockerImage} ."
     //sh "docker build -t ${dockerImage} ."
 }

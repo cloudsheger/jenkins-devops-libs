f040775b2873f71b190fd8c32ee86d9c490c384f
cloudsheger <cloudsheger@gmail.com>
2023-12-10 03:49:11 -0500

    dockerfile path updated

---
+++ vars/docker.groovy
@@ -0,0 +1,18 @@
+void dockerBuild(Map config) {
+    def dockerImage = "${config.DOCKER_REGISTRY}/${config.DOCKER_REPO}/${config.IMAGE_NAME}:${config.BUILD_NUMBER}"
+    // Build Docker image
+    sh "docker build -f dockerfiles/Dockerfile.packer -t ${dockerImage} ."
+    //sh "docker build -t ${dockerImage} ."
+}
+
+void dockerPush(Map config) {
+    def imageName = "${config.DOCKER_REGISTRY}/${config.DOCKER_REPO}/${config.IMAGE_NAME}:${config.BUILD_NUMBER}"
+
+    echo "Building Docker image..."
+
+    // Use Docker Pipeline plugin to authenticate with Docker registry
+    docker.withRegistry(config.DOCKER_REGISTRY_URL, config.DOCKER_CREDS_ID) {
+        sh "docker build -f dockerfiles/Dockerfile.packer -t ${imageName} ."
+        sh "docker push ${imageName}"
+    }
+}
